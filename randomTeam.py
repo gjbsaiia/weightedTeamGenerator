8090defe062ae7692f94eb2c3922b391a15faf03
Griffin <35904717+gjbsaiia@users.noreply.github.com>
2018-05-31 05:30:29 -0400

    first attempt

--- randomTeam.py
+++ randomTeam.py
@@ -1,73 +1,98 @@
 import os
 import sys
+import numpy as np
 
- teamSize = 5
+teamSize = 5
 
- def getTarget(weights):
-	 totalWeight = 0
-	 for each weight in weights:
-		 totalWeight += weight
-	 return ( totalWeight / len(weights) )
+def getNoOfClusters(listing, teamSize):
+	 totalPeople = len(listing)
+	 n = len(listing)/teamSize
+	 m = len(listing)/teamSize
+	 if(n == m):
+		 return n
 
- def countingSort(weights, names, exp):
-    n = len(weights)
-    wOut = [0] * (n)
-	nOut = []
-    count = [0] * (10)
-    for i in range(0, n):
-        index = ( weights[i] / exp )
-        count[ ( index )%10 ] += 1
-    for i in range(1,10):
-        count[i] += count[ i - 1 ]
-    i = n - 1
-    while i >= 0:
-        index = ( weights[i] / exp )
-        wOut[ count[ ( index )%10 ] - 1 ] = weights[i]
-		nOut[ count[ ( index )%10 ] - 1 ] = names[i]
-        count[ ( index )%10 ] -= 1
-        i -= 1
-    i = 0
-    for i in range(0, len(weights)):
-        weight[i] = wOut[i]
-		names[i] = nOut[i]
+ 	 else:
+		 if(teamSize == 5):
+		 	teamSize = 6
+		 	getNoOfClusters(listing,teamSize)
+	 	 else:
+		 	return n + 1
 
- def radixSort(weights, names):
-    maximum = max(arr)
-    exp = 1
-    while max1/exp > 0:
-        countingSort(weights, names, exp)
-        exp *= 10
+def dist(new, centroid):
+	 return ((centroid - new) ** 2)
 
+def allocateTeams(clusters, centers, listing):
+	 for each in listing:
+		cut = listing.split(' ')
+		weight = cut[1]
+		name = cut[0]
+	 	min = 10
+		nm = 0
+		i = 0
+	 	for center in centers:
+			d = dist(weight, center)
+		 	if (d < min & (len(cluster[i]) < teamSize)):
+				min = d
+				nm = i
+			i += 1
+		clusters[i].append(listing)
+		centers[i] = recenter(centers[i], clusters[i])
 
+def maxDeviation(clusters):
+	 maxAve = 10
+	 for cluster in clusters:
+		 total = 0
+		 for member in cluster:
+			 cut = member.split(' ')
+			 weight = cut[1]
+			 total += weight
+		 ave = (total+0.0) / len(cluster)
+		 if(ave > maxAve):
+			 maxAve = ave
+	 return maxAve
 
- def main():
-	 print "welcome to 'random' tug o' war team generator"
-	 line = " "
-	 listing = []
-	 names = []
-	 weights = []
-	 teams = []
-	 while line != "done":
-	 	line = raw_input("please enter a name and a weight or done: ")
+def recenter(i, center, cluster):
+	ave = 0
+	for entry in cluster:
+		cut = entry.split(' ')
+		weight = cut[1]
+		ave += weight
+	return (ave+0.0)/len(cluster)
+
+def cleanUpClusters(clusters):
+	for cluster in cluster:
+		team = []
+		for entry in cluster:
+			cut = entry.split(' ')
+			name = cut[0]
+			team.append(name)
+		cluster = team
+
+def main():
+	print "welcome to 'random' tug o' war team generator"
+ 	line = " "
+ 	listing = []
+ 	clusters = []
+ 	while line != "done":
+ 		line = raw_input("please enter a name and a weight or done: ")
 		listing.append(line)
-		split = line.split(' ')
-		names.append(split[0])
-		weights.append(split[1])
-	targetWeight = getTarget(weights)
-	noOfTeams = (len(names) / teamSize)
-	radixSort(weights)
-	allocateTeams(teams, weights, names)
-
-
-
-
-
+	i = 0
+	while(i < getNoOfClusters(listing, teamSize)):
+		clusters.append([])
+ 	centers = np.zeros(getNoOfClusters(listing, teamSize))
+	allocateTeams(clusters, centers, listing)
+ 	deviation = maxDeviation(clusters)
+	while(deviation > 2):
+		allocateTeams(clusters, centers, listing)
+		deviation = maxDeviation(clusters)
+	cleanUpClusters(clusters)
+	print clusters
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print 'Interrupted'
+	try:
+    	main()
+	except KeyboardInterrupt:
+    	print 'Interrupted'
         try:
             sys.exit(0)
         except SystemExit:
