c36d1b425d4a606b2ed06d865341348bf60ddf4b
Griffin <35904717+gjbsaiia@users.noreply.github.com>
2018-07-02 12:21:17 +0000

    update

--- balancedTeam.py
+++ balancedTeam.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 
-teamSize = 5
+teamSize = 7
 
 def getNoOfClusters(listing, teamSize):
     #totalPeople = len(listing)
@@ -47,7 +47,7 @@ def maxDeviation(clusters):
 
 def aveInCluster(cluster):
      total = 0
-     print(cluster)
+     #print(cluster)
      for member in cluster:
          cut = member.split(' ')
          weight = int(cut[1])
@@ -80,7 +80,6 @@ def initCentroids(numCentroids):
     return centroids
 
 def main():
-    filename = "listing.txt"
     print("welcome to balanced tug o' war team generator")
     line = " "
     listing = []
@@ -102,6 +101,7 @@ def main():
             listing.append(line)
             line = input("please enter a name and a weight, or done: ")
     else:
+        filename = input("Please enter filename: ")
         with open(filename) as names:
             lines = names.readlines()
             names.close()
@@ -116,16 +116,18 @@ def main():
         centers.append(0)
         i += 1
     allocateTeams(clusters, centers, listing)
-    print('clusters post alloc: ')
     deviation = maxDeviation(clusters)
     print("First round:")
     print(clusters)
     print(deviation)
-    while(deviation > 2):
-        print('reach')
-        print(clusters)
+    i = 0
+    while(deviation > 3):
         allocateTeams(clusters, centers, listing)
         deviation = maxDeviation(clusters)
+        i += 1
+        print('round '+str(i)+':')
+        print(deviation)
+        print(clusters)
     cleanUpClusters(clusters)
     print(deviation)
     print(clusters)
